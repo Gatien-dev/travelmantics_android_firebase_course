4d0539dd3743686ff0e733a0d0393853b4398b8c
Gatien GNAKOU-EDJAMBO <gatiengnakou@gmail.com>
2019-08-04 12:29:39 +0000

    Add the firebaseUtils class

--- Travelmantics/app/src/main/java/com/example/travelmantics/InsertActivity.java
+++ Travelmantics/app/src/main/java/com/example/travelmantics/InsertActivity.java
@@ -23,8 +23,9 @@ public class InsertActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insert);
-        mFirebaseDatabase=FirebaseDatabase.getInstance();
-        mDatabaseReference=mFirebaseDatabase.getReference().child("traveldeals");
+        FirebaseUtil.openFbReference("traveldeals");
+        mFirebaseDatabase=FirebaseUtil.mFirebaseDatabase;
+        mDatabaseReference=FirebaseUtil.mDatabaseReference;
         txtTitle=findViewById(R.id.txtTitle);
         txtPrice=findViewById(R.id.txtPrice);
         txtDescription=findViewById(R.id.txtDescription);
--- Travelmantics/app/src/main/java/com/example/travelmantics/TravelDeal.java
+++ Travelmantics/app/src/main/java/com/example/travelmantics/TravelDeal.java
@@ -7,7 +7,11 @@ public class TravelDeal {
     private String price;
     private String imageUrl;
 
-    public TravelDeal( String title, String description, String price, String imageUrl) {
+    public TravelDeal() {
+
+    }
+
+    public TravelDeal(String title, String description, String price, String imageUrl) {
 
         this.setTitle(title);
         this.setDescription(description);
